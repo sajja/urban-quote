102d9c746bdd1d887a8b1b64041f472196271e8f
Sajith Silva <sajiths@pagero.com>
2018-08-09 10:54:05 +0530

    more stuff

--- test.php
+++ test.php
@@ -1,11 +1,23 @@
 
 <?php
+	$filename= fopen('/home/sajith/Downloads/tmp/x1.mp4', 'rb');
 
-// Show all information, defaults to INFO_ALL
-phpinfo();
-
-// Show just the module information.
-// phpinfo(8) yields identical results.
-phpinfo(INFO_MODULES);
-
+// here you can chech your sessions
+ 
+function flush_buffers(){
+    ob_end_flush();
+    ob_flush();
+    flush();
+    ob_start();
+}
+ 
+    header("Content-Type: video/mp4");
+    //header("Content-Disposition: attachment; filename=video.flv");
+    $fd = fopen($filename, "r");
+    while(!feof($fd)) {
+        echo fread($fd, 1024 * 5);
+	    flush_buffers();
+        }
+    fclose ($fd);
+    exit();
 ?>
